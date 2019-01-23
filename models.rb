3e1407cc3154d4ffcd2f283db22eb8a9fe963463
Johnny Kim <spyderkim@gmail.com>
2019-01-23 18:29:55 -0500

    fixed models.rb

--- models.rb
+++ models.rb
@@ -1,6 +1,7 @@
 require "sinatra/activerecord"
 require "pg"
 
+configure :development do
   set :database, {
     adapter: "postgresql",
     host: "localhost",
@@ -8,6 +9,13 @@ require "pg"
     username: "postgres",
     password: "postgrespassword"
   }
+end
+
+configure :production do
+  # this environment variable is auto generated/set by heroku
+  #   check Settings > Reveal Config Vars on your heroku app admin panel
+  set :database, ENV["DATABASE_URL"]
+end
 
 class User < ActiveRecord::Base
   # only one user can have their username
